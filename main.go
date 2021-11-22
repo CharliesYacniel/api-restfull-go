b3d5ba61853531b34e3364893d59be7e29ef58e0
Charlies Yacniel <charliesyacniel@gmail.com>
2021-11-22 00:41:13 -0600

    Charlies|22111021|implementacion de servicios par alos porgramas a ejeutar

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-
-	"github.com/dgraph-io/dgo/v200"
-	"github.com/dgraph-io/dgo/v200/protos/api"
-	"google.golang.org/grpc"
 )
-
-type Link struct {
-	Uid   string   `json:"uid,omitempty"`
-	URL   string   `json:"url,omitempty"`
-	DType []string `json:"dgraph.type,omitempty"`
-}
-type CancelFunc func()
 
 func main() {
 	port := "3000"
@@ -32,12 +18,6 @@ func main() {
 
 	log.Printf("API KENIA en LINEA = http://localhost:%s", port)
 
-	//CONEXION BASE DE DATOS
-	getDgraphClient()
-
-	//CONSULTAS A BD
-	ExampleTxn_Mutate()
-
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -45,230 +25,7 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("API HOME!"))
 	})
-
-	r.Mount("/posts", postsResource{}.Routes())
 	r.Mount("/programs", programsResource{}.Routes())
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
-}
-
-func getDgraphClient() (*dgo.Dgraph, CancelFunc) {
-	conn, err := grpc.Dial("127.0.0.1:9080", grpc.WithInsecure())
-	if err != nil {
-		log.Fatal("While trying to dial gRPC")
-	}
-
-	dc := api.NewDgraphClient(conn)
-	dg := dgo.NewDgraphClient(dc)
-	log.Printf("CONEXION EXITOSA CON DGRAPHQL")
-	// ctx := context.Background()
-
-	// Perform login call. If the Dgraph cluster does not have ACL and
-	// enterprise features enabled, this call should be skipped.
-	// for {
-	// 	// Keep retrying until we succeed or receive a non-retriable error.
-	// 	// err = dg.Login(ctx, "groot", "password")
-	// 	// err = dg.Login(ctx, "groot", "password")
-	// 	// err = dg.Login(ctx)
-	// 	// fmt.Println(err)
-	// 	if err == nil || !strings.Contains(err.Error(), "Please retry") {
-	// 		break
-	// 	}
-	// 	time.Sleep(time.Second)
-	// }
-	// if err != nil {
-	// 	log.Fatalf("While trying to login %v", err.Error())
-	// }
-
-	return dg, func() {
-		if err := conn.Close(); err != nil {
-			log.Printf("Error while closing connection:%v", err)
-		}
-
-	}
-}
-
-func ExampleTxn_Mutate() {
-	type School struct {
-		Name  string   `json:"name,omitempty"`
-		DType []string `json:"dgraph.type,omitempty"`
-	}
-
-	type loc struct {
-		Type   string    `json:"type,omitempty"`
-		Coords []float64 `json:"coordinates,omitempty"`
-	}
-
-	// If omitempty is not set, then edges with empty values (0 for int/float, "" for string, false
-	// for bool) would be created for values not specified explicitly.
-
-	type Person struct {
-		Uid      string   `json:"uid,omitempty"`
-		Name     string   `json:"name,omitempty"`
-		Age      int      `json:"age,omitempty"`
-		Married  bool     `json:"married,omitempty"`
-		Raw      []byte   `json:"raw_bytes,omitempty"`
-		Friends  []Person `json:"friends,omitempty"`
-		Location loc      `json:"loc,omitempty"`
-		School   []School `json:"school,omitempty"`
-		DType    []string `json:"dgraph.type,omitempty"`
-	}
-
-	dg, cancel := getDgraphClient()
-	defer cancel()
-	// While setting an object if a struct has a Uid then its properties in the
-	// graph are updated else a new node is created.
-	// In the example below new nodes for Alice, Bob and Charlie and school
-	// are created (since they don't have a Uid).
-	p := Person{
-		Uid:     "_:alice",
-		Name:    "Alice",
-		Age:     26,
-		Married: true,
-		DType:   []string{"Person"},
-		Location: loc{
-			Type:   "Point",
-			Coords: []float64{1.1, 2},
-		},
-		Raw: []byte("raw_bytes"),
-		Friends: []Person{{
-			Name:  "Bob",
-			Age:   24,
-			DType: []string{"Person"},
-		}, {
-			Name:  "Charlie",
-			Age:   29,
-			DType: []string{"Person"},
-		}},
-		School: []School{{
-			Name:  "Crown Public School",
-			DType: []string{"Institution"},
-		}},
-	}
-
-	op := &api.Operation{}
-	op.Schema = `
-		   name: string @index(exact) .
-		   age: int .
-		   married: bool .
-		   Friends: [uid] .
-		   loc: geo .
-		   type: string .
-		   coords: float .
-		   Friends: [uid] .
-   
-		   type Person {
-			   name
-			   age
-			   married
-			   Friends
-			   loc
-			 }
-   
-		   type Institution {
-			   name
-		   }
-	   `
-
-	ctx := context.Background()
-	if err := dg.Alter(ctx, op); err != nil {
-		log.Fatal(err)
-	}
-
-	mu := &api.Mutation{
-		CommitNow: true,
-	}
-	pb, err := json.Marshal(p)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mu.SetJson = pb
-	response, err := dg.NewTxn().Mutate(ctx, mu)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Assigned uids for nodes which were created would be returned in the response.Uids map.
-	puid := response.Uids["alice"]
-	const q = `
-		   query Me($id: string){
-			   me(func: uid($id)) {
-				   name
-				   age
-				   loc
-				   raw_bytes
-				   married
-				   dgraph.type
-				   friends @filter(eq(name, "Bob")) {
-					   name
-					   age
-					   dgraph.type
-				   }
-				   school {
-					   name
-					   dgraph.type
-				   }
-			   }
-		   }
-	   `
-
-	variables := make(map[string]string)
-	variables["$id"] = puid
-	resp, err := dg.NewTxn().QueryWithVars(ctx, q, variables)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	type Root struct {
-		Me []Person `json:"me"`
-	}
-
-	var r Root
-	err = json.Unmarshal(resp.Json, &r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out, _ := json.MarshalIndent(r, "", "\t")
-	fmt.Printf("%s\n", out)
-	// Output: {
-	// 	"me": [
-	// 		{
-	// 			"name": "Alice",
-	// 			"age": 26,
-	// 			"married": true,
-	// 			"raw_bytes": "cmF3X2J5dGVz",
-	// 			"friends": [
-	// 				{
-	// 					"name": "Bob",
-	// 					"age": 24,
-	// 					"loc": {},
-	// 					"dgraph.type": [
-	// 						"Person"
-	// 					]
-	// 				}
-	// 			],
-	// 			"loc": {
-	// 				"type": "Point",
-	// 				"coordinates": [
-	// 					1.1,
-	// 					2
-	// 				]
-	// 			},
-	// 			"school": [
-	// 				{
-	// 					"name": "Crown Public School",
-	// 					"dgraph.type": [
-	// 						"Institution"
-	// 					]
-	// 				}
-	// 			],
-	// 			"dgraph.type": [
-	// 				"Person"
-	// 			]
-	// 		}
-	// 	]
-	// }
-
 }
--- programs.go
+++ programs.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	//   "context"
+
 	"bufio"
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os/exec"
 
+	"github.com/dgraph-io/dgo/v200"
+	"github.com/dgraph-io/dgo/v200/protos/api"
 	"github.com/go-chi/chi"
-	// "github.com/go-chi/chi"
+	"google.golang.org/grpc"
 )
 
 type programsResource struct{}
+type CancelFunc func()
 
 type responseData struct {
 	Status  bool   `json:"Status"`
@@ -24,22 +31,64 @@ type responseData struct {
 func (rs programsResource) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Get("/getAll", rs.List) // GET /programs - Read a list of programs.
-	//   route.Post("/", rs.Create) // POST /programs - Create a new post.
-
-	//   route.Route("/{id}", func(r chi.Router) {
-	//     route.Use(PostCtx)
-	//     route.Get("/", rs.Get)       // GET /programs/{id} - Read a single post by :id.
-	//     route.Put("/", rs.Update)    // PUT /programs/{id} - Update a single post by :id.
-	//     route.Delete("/", rs.Delete) // DELETE /programs/{id} - Delete a single post by :id.
-	//   })
+	r.Get("/getAll", rs.GetAll)
+	r.Get("/getById", rs.GetById)
+	r.Get("/create", rs.Create)
+	r.Get("/execute", rs.Execute)
 
 	return r
 }
 
 // Request Handler - GET /AllPrograms - Read a list of programs.
-func (rs programsResource) List(w http.ResponseWriter, r *http.Request) {
+func (rs programsResource) GetAll(w http.ResponseWriter, r *http.Request) {
+	dg, cancel := getDgraphClient()
+	defer cancel()
+	type Programs struct {
+		Uid          string `json:"uid,omitempty"`
+		Name         string `json:"name,omitempty"`
+		Codetex      string `json:"codetext,omitempty"`
+		Language     string `json:"language,omitempty"`
+		User         string `json:"user,omitempty"`
+		CodeCompiled string `json:"codecompiled,omitempty"`
+	}
+	ctx := context.Background()
 
+	variables := make(map[string]string)
+	q := `
+	{
+		getAllPrograms(func: has(nameProgram)) {
+		  nameProgram
+		  user
+		  language
+		  codeTex
+		  codeCompiled
+		}
+	  }
+	   `
+	resp, err := dg.NewTxn().QueryWithVars(ctx, q, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+	type Root struct {
+		Me []Programs `json:"me"`
+	}
+	err = json.Unmarshal(resp.Json, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(resp.Json))
+}
+func (rs programsResource) GetById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("Obtener por ID progra,"))
+}
+func (rs programsResource) Create(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("CREATE progra,"))
+}
+
+func (rs programsResource) Execute(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command("python3", "./assets/game.py")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -53,43 +102,73 @@ func (rs programsResource) List(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-
 	go copyOutput(stdout)
 	go copyOutput(stderr)
 	cmd.Wait()
-
-	// res2D := &responseData{
-	// 	Status:  true,
-	// 	Code:    200,
-	// 	Object:  `{"id": 1,"code": "n1 = float(input('Intro número uno: ')) n2 = float(input('Intro numero dos: ')) suma = n1+n2 print('La suma es: ', suma) n3 = float(input('la resuma:')) l = suma+n3 print('la resuma es:', l)"}`,
-	// 	Message: `datos obtenidos con exito`,
-	// }
-
-	// res2B, _ := json.Marshal(res2D)
-	// fmt.Println(string(res2B))
-
-	//   resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-	//   resp = res2D
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	defer resp.Body.Close()
-
 	w.Header().Set("Content-Type", "application/json")
-
-	if _, err := io.Copy(w, resp.Body); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	w.Write([]byte("Excute program"))
 }
 
+//ejecutar programs
 func copyOutput(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
+}
+
+// COnexion Grapgh QL
+func getDgraphClient() (*dgo.Dgraph, CancelFunc) {
+	conn, err := grpc.Dial("127.0.0.1:9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("While trying to dial gRPC")
+	}
+	dc := api.NewDgraphClient(conn)
+	dg := dgo.NewDgraphClient(dc)
+	log.Printf("CONEXION EXITOSA CON DGRAPHQL")
+	return dg, func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Error while closing connection:%v", err)
+		}
+	}
+}
+func getAllPrograms() {
+	dg, cancel := getDgraphClient()
+	defer cancel()
+	type Programs struct {
+		Uid          string `json:"uid,omitempty"`
+		Name         string `json:"name,omitempty"`
+		Codetex      string `json:"codetext,omitempty"`
+		Language     string `json:"language,omitempty"`
+		User         string `json:"user,omitempty"`
+		CodeCompiled string `json:"codecompiled,omitempty"`
+	}
+	ctx := context.Background()
+
+	variables := make(map[string]string)
+	q := `
+	{
+		getAllPrograms(func: has(nameProgram)) {
+		  nameProgram
+		  user
+		  language
+		  codeTex
+		  codeCompiled
+		}
+	  }
+	   `
+	resp, err := dg.NewTxn().QueryWithVars(ctx, q, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+	type Root struct {
+		Me []Programs `json:"me"`
+	}
+	var r Root
+	err = json.Unmarshal(resp.Json, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(resp.Json))
+
 }
