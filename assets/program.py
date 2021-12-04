d6ac33ff9f84af9c4a05203151ffb0cd50f18e2d
Charlies Yacniel <charliesyacniel@gmail.com>
2021-12-04 02:13:55 -0600

    Charlies|04122021|creando primer programa usando 4 nodos

--- assets/program.py
+++ assets/program.py
@@ -1,10 +1,10 @@
-import sys
-i=0
-while i<10:
-    print ("Hello")
-    sys.stdout.flush()
-    i=i+1
-n1 = 3
-n2 = 10
-suma = n1+n2
-print("La suma es: ", suma)
+import sys 
+
+
+
+
+input_1 = 100
+input_2 = 14.5
+
+assing = input_1 + input_2
+print("Assing is: ", assing)
